80fb16af47ee35d5f7f5b355b50f3416fe9c8644
LJW0401 <114296633+LJW0401@users.noreply.github.com>
2022-12-24 17:13:03 +0800

    Update 冒泡排序.cpp

--- 排序算法/冒泡排序.cpp
+++ 排序算法/冒泡排序.cpp
@@ -3,9 +3,9 @@
 #include<stdlib.h>
 
 /*基础版冒泡排序*/
-void Sort_Bubble(int a[], int n){
-	for(int i=0;i<n-1;i++){
-		for(int j=n-1;j>i;j--){
+void Sort_Bubble(int a[], long n){
+	for(long i=0;i<n-1;i++){
+		for(long j=n-1;j>i;j--){
 			if(a[j-1]>a[j]){
 				int t=a[j];a[j]=a[j-1];a[j-1]=t;
 			}
@@ -14,11 +14,11 @@ void Sort_Bubble(int a[], int n){
 }
 
 /*第1次改进，没有交换时停止*/
-void Sort_Bubble_I(int a[], int n){
+void Sort_Bubble_I(int a[], long n){
 	int flag=0;
 	
-	for(int i=0;i<n-1;i++){
-		for(int j=n-1;j>i;j--){
+	for(long i=0;i<n-1;i++){
+		for(long j=n-1;j>i;j--){
 			if(a[j-1]>a[j]){
 				int t=a[j];a[j]=a[j-1];a[j-1]=t;
 				flag=1;
@@ -29,19 +29,19 @@ void Sort_Bubble_I(int a[], int n){
 }
 
 /*第2次改进，双向冒泡，一次确定2个值*/
-void Sort_Bubble_II(int a[], int n){
+void Sort_Bubble_II(int a[], long n){
 	int flag=0;
 	
-	int l=0,r=n-1;
+	long l=0,r=n-1;
 	while(l<r){
-		for(int i=l;i<r;i++){
+		for(long i=l;i<r;i++){
 			if(a[i]>a[i+1]){
 				int t=a[i];a[i]=a[i+1];a[i+1]=t;
 				flag=1;
 			}
 		}
 		r--;
-		for(int i=r-1;i>=l;i--){
+		for(long i=r-1;i>=l;i--){
 			if(a[i]>a[i+1]){
 				int t=a[i];a[i]=a[i+1];a[i+1]=t;
 				flag=1;
@@ -61,7 +61,7 @@ int main(){
 	
 	printf("---冒泡排序---\n");
 	printf("数据量：%d\n\n",MAX);
-	for(int i=0;i<MAX;i++){
+	for(long i=0;i<MAX;i++){
 		array[0][i]=rand()%10000;
 		array[1][i]=rand()%10000;
 		array[2][i]=rand()%10000;
@@ -80,12 +80,12 @@ int main(){
 	printf("冒泡排序改进1次运行时间: %d ms\n", delta);
 	
 	start = clock();
-	Sort_Bubble_II(array[2],MAX);
+	Sort_Bubble_II(array[2],15);
 	finish = clock();
 	delta=finish-start;
 	printf("冒泡排序改进2次运行时间: %d ms\n", delta);
 	
-//	for(int i=0;i<20;i++){
+//	for(int i=0;i<15;i++){
 //		printf("%d ",array[2][i]);
 //	}
 }
