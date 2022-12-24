3018f14572aefb2255b9cd4a35578384b84c823d
LJW0401 <114296633+LJW0401@users.noreply.github.com>
2022-12-24 17:14:47 +0800

    Update 选择排序.cpp

--- 排序算法/选择排序.cpp
+++ 排序算法/选择排序.cpp
@@ -1,16 +1,14 @@
 #include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
 
-void sort(int a[], int n, int reverse = 0){
-	int sign;
-	if(reverse==0)sign=1;
-	else sign=-1;
-	
-	for(int i=0;i<n-1;i++){
-		int k = i;
-		for(int j=i+1;j<n;j++){
-			if(sign*a[k]>sign*a[j]){
+/*基础版选择排序*/
+void Sort_Choose(int a[], long n){
+	for(long i=0;i<n-1;i++){
+		long k = i;
+		for(long j=i+1;j<n;j++){
+			if(a[k]>a[j])
 				k=j;
-			}
 		}
 		if(k!=i){
 			int t=a[i];a[i]=a[k];a[k]=t;
@@ -18,13 +16,62 @@ void sort(int a[], int n, int reverse = 0){
 	}
 }
 
-int main(){
-	int a[10]={13,8,74,9,6,78,25,74,13,84};
-	for(int i=0;i<10;i++){
-		printf("%d ",a[i]);
-	}printf("\n");
-	sort(a,10);
-	for(int i=0;i<10;i++){
-		printf("%d ",a[i]);
+/*改进1，在一次选择中同时确定最大和最小值*/
+void Sort_Choose_I(int a[], long n){
+	long l=0,r=n-1;
+	while(l<r){
+		long p=l,q=r;
+		
+		for(long i=l;i<=r;i++){
+			if(a[p]>a[i]){
+				p=i;
+			}
+			if(a[q]<a[i]){
+				q=i;
+			}
+		}
+		if(l!=p){
+			int t=a[p];a[p]=a[l];a[l]=t;
+		}
+		if(q==l){
+			q=p;
+		}
+		if(r!=q){
+			int t=a[q];a[q]=a[r];a[r]=t;
+		}
+		r--;l++;
 	}
+}
+
+#define NUM 100000
+int main(){
+	clock_t start, finish;
+	int delta;
+	srand(time(NULL));
+	int array[3][NUM];
+	
+	printf("---选择排序---\n");
+	printf("数据量：%d\n\n",NUM);
+	for(long i=0;i<NUM;i++){
+		array[0][i]=rand()%10000;
+		array[1][i]=rand()%10000;
+		array[2][i]=rand()%10000;
+	}
+	
+	start = clock();
+	Sort_Choose(array[0],NUM);
+	finish = clock();
+	delta=finish-start;
+	printf("选择排序改进0次序运行时间: %d ms\n", delta);
+	
+	start = clock();
+	Sort_Choose_I(array[1],NUM);
+	finish = clock();
+	delta=finish-start;
+	printf("选择排序改进1次序运行时间: %d ms\n", delta);
+
+	
+//	for(int i=0;i<10;i++){
+//		printf("%d ",array[1][i]);
+//	}
 }
