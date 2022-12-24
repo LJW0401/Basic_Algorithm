1245da4342356b3e26adf64e3ac0b488d5336cd1
LJW0401 <114296633+LJW0401@users.noreply.github.com>
2022-12-24 12:46:53 +0800

    Add files via upload

--- 排序算法/冒泡排序.cpp
+++ 排序算法/冒泡排序.cpp
@@ -1,26 +1,91 @@
 #include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
 
-void sort(int a[], int n, int reverse = 0){
-	int sign;
-	if(reverse==0)sign=1;
-	else sign=-1;
-	
+/*基础版冒泡排序*/
+void Sort_Bubble(int a[], int n){
 	for(int i=0;i<n-1;i++){
 		for(int j=n-1;j>i;j--){
-			if(sign*a[j-1]>sign*a[j]){
+			if(a[j-1]>a[j]){
 				int t=a[j];a[j]=a[j-1];a[j-1]=t;
 			}
 		}
 	}
 }
 
-int main(){
-	int a[10]={13,8,74,9,6,78,25,74,13,84};
-	for(int i=0;i<10;i++){
-		printf("%d ",a[i]);
-	}printf("\n");
-	sort(a,10);
-	for(int i=0;i<10;i++){
-		printf("%d ",a[i]);
+/*第1次改进，没有交换时停止*/
+void Sort_Bubble_I(int a[], int n){
+	int flag=0;
+	
+	for(int i=0;i<n-1;i++){
+		for(int j=n-1;j>i;j--){
+			if(a[j-1]>a[j]){
+				int t=a[j];a[j]=a[j-1];a[j-1]=t;
+				flag=1;
+			}
+		}
+		if(!flag) break;
 	}
+}
+
+/*第2次改进，双向冒泡，一次确定2个值*/
+void Sort_Bubble_II(int a[], int n){
+	int flag=0;
+	
+	int l=0,r=n-1;
+	while(l<r){
+		for(int i=l;i<r;i++){
+			if(a[i]>a[i+1]){
+				int t=a[i];a[i]=a[i+1];a[i+1]=t;
+				flag=1;
+			}
+		}
+		r--;
+		for(int i=r-1;i>=l;i--){
+			if(a[i]>a[i+1]){
+				int t=a[i];a[i]=a[i+1];a[i+1]=t;
+				flag=1;
+			}
+		}
+		l++;
+		if(!flag) break;
+	}
+}
+
+#define MAX 5000
+int main(){
+	clock_t start, finish;
+	int delta;
+	srand(time(NULL));
+	int array[3][MAX];
+	
+	printf("---冒泡排序---\n");
+	printf("数据量：%d\n\n",MAX);
+	for(int i=0;i<MAX;i++){
+		array[0][i]=rand()%10000;
+		array[1][i]=rand()%10000;
+		array[2][i]=rand()%10000;
+	}
+	
+	start = clock();
+	Sort_Bubble(array[0],MAX);
+	finish = clock();
+	delta=finish-start;
+	printf("原始冒泡排序运行时间: %d ms\n", delta);
+	
+	start = clock();
+	Sort_Bubble_I(array[1],MAX);
+	finish = clock();
+	delta=finish-start;
+	printf("冒泡排序改进1次运行时间: %d ms\n", delta);
+	
+	start = clock();
+	Sort_Bubble_II(array[2],MAX);
+	finish = clock();
+	delta=finish-start;
+	printf("冒泡排序改进2次运行时间: %d ms\n", delta);
+	
+//	for(int i=0;i<20;i++){
+//		printf("%d ",array[2][i]);
+//	}
 }
